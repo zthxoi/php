4f88dd6f5e85f182cba0ceb8bf0415a07b5aac8c
zthxoi <den.yurchenko.05@mail.ru>
2025-12-23 17:10:25 +0300

    Update SuperUser.php

--- lab7/src/Classes/SuperUser.php
+++ lab7/src/Classes/SuperUser.php
@@ -4,19 +4,49 @@ namespace src\Classes;
 
 require_once 'User.php';
 
+/**
+ * Класс SuperUser представляет пользователя с расширенными правами
+ * 
+ * Наследует функциональность класса User и добавляет свойство role (роль).
+ * Переопределяет метод showInfo() для отображения дополнительной информации.
+ * 
+ * @package src\Classes
+ * @extends User
+ */
 class SuperUser extends User
 {
-    public $role;
-
-    
-    public function __construct($name, $login, $password, $role)
-    {
+    /**
+     * Конструктор класса SuperUser с продвижением параметров
+     * 
+     * Расширяет конструктор родительского класса, добавляя параметр role.
+     * Вызывает конструктор родительского класса для инициализации базовых свойств.
+     * Свойство role инициализируется через продвижение параметров.
+     * 
+     * @param string $name Имя пользователя
+     * @param string $login Логин пользователя
+     * @param string $password Пароль пользователя
+     * @param string $role Роль пользователя в системе
+     */
+    public function __construct(
+        string $name,
+        string $login,
+        string $password,
+        public string $role
+    ) {
         parent::__construct($name, $login, $password);
-        $this->role = $role;
+        // Свойство role уже инициализировано благодаря продвижению параметров
     }
 
-    
-    public function showInfo(): string  
+    /**
+     * Возвращает HTML-строку с полной информацией о супер-пользователе
+     * 
+     * Переопределяет метод родительского класса, добавляя отображение роли.
+     * Формирует оформленный HTML-блок с именем, логином и ролью пользователя.
+     * 
+     * @return string HTML-строка с полной информацией о пользователе
+     * @override
+     */
+    public function showInfo(): string
     {
         return "<div class=\"super-user-info\">
                     <h3>Super User Info</h3>
