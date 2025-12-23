2ed8722c052e1becd1f0d6b77dba82b5ce40d6e2
zthxoi <den.yurchenko.05@mail.ru>
2025-12-23 17:02:16 +0300

    Update functions.php

--- lab3/functions.php
+++ lab3/functions.php
@@ -2,18 +2,25 @@
 declare(strict_types=1);
 
 $extensions = get_loaded_extensions();
+$totalFunctions = 0;
 
 foreach ($extensions as $ext) {
     $functions = get_extension_funcs($ext);
+    $functionCount = $functions ? count($functions) : 0;
+    $totalFunctions += $functionCount;
 
-    echo "<h2>Модуль: {$ext}</h2>";
+    echo "<h2>Модуль: {$ext} ({$functionCount} функций)</h2>";
+    
     if ($functions) {
         echo '<ul>';
-
-        foreach ($functions as $func)
-            echo "<li>{$func}";
-
+        foreach ($functions as $func) {
+            echo "<li>{$func}</li>";
+        }
         echo '</ul>';
-    } 
-    else echo '<p>Нет доступных функций для модуля';
+    } else {
+        echo '<p>Нет доступных функций для модуля</p>';
+    }
 }
+
+echo "<h3>Итого: {$totalFunctions} функций в " . count($extensions) . " модулях</h3>";
+?>
